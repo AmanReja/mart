ff2b6dd1f85f930db712167089351244246da037
Aman Reja <amnmolla742@gmail.com>
2025-07-15 10:28:44 +0530

    fix

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,32 +1,26 @@
 import React from "react";
+import phone from "../assets/icons/phone.svg";
 
 const Navbar = () => {
   return (
     <>
-      <div className="flex w-full h-[71px] bg-transparent justify-between sm:justify-evenly items-center px-[80px] sticky top-[5px] ">
+      <div className="flex w-full h-[71px] bg-transparent justify-between sm:justify-evenly items-center px-[20px] sticky top-[5px] ">
         <h1 className="text-4xl">Martspell</h1>
         <div className=" w-[800px] justify-center items-center gap-[20px] text-[18px] hidden sm:flex">
           <a href="">Home</a>
           <a href="">Services</a>
           <a href="">About Us</a>
-          <a href="">Clint case studies</a>
+
           <a href="">Contact us</a>
-          <a href="">Career</a>
-          <a href="">Shop</a>
-          <a href="">Pay now</a>
         </div>
         <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            id="search"
-            fill="#fff"
+          <a
+            className="text-white flex justify-center items-center bg-yellow-500 p-2 rounded-full text-2xl"
+            href="tell-7908854829"
           >
-            <path d="M9 18c-5 0-9-4-9-9s4-9 9-9 9 4 9 9-4 9-9 9zM9 2C5.1 2 2 5.1 2 9s3.1 7 7 7 7-3.1 7-7-3.1-7-7-7z"></path>
-            <path d="M23 24c-.3 0-.5-.1-.7-.3l-8-8c-.4-.4-.4-1 0-1.4s1-.4 1.4 0l8 8c.4.4.4 1 0 1.4-.2.2-.4.3-.7.3z"></path>
-          </svg>
+            <div className="hidden sm:block">Call Us</div>
+            <img className="w-[40px] h-[40px]" src={phone} alt="" />
+          </a>
         </div>
       </div>
     </>
