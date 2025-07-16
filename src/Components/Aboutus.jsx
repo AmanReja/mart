9e874366d2676440bd23e16f543d0ec8254d0197
Aman Reja <amnmolla742@gmail.com>
2025-07-16 15:21:43 +0530

    fix

--- src/Components/Aboutus.jsx
+++ src/Components/Aboutus.jsx
@@ -1,39 +1,304 @@
 import React from "react";
+import Seo from "../assets/digital/seo.svg";
+import Containt from "../assets/digital/content.svg";
 
 const Aboutus = () => {
   return (
     <>
-      <section className="py-24 relative">
-        <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
-          <div className="w-full justify-start items-center gap-8 grid lg:grid-cols-2 grid-cols-1">
-            <div className="w-full flex-col justify-start lg:items-start items-center gap-10 inline-flex">
-              <div className="w-full flex-col justify-start lg:items-start items-center gap-4 flex">
-                <h2 className="text-white text-4xl font-bold font-manrope leading-normal lg:text-start text-center">
-                  Building Stronger Communities through Collaboration and
-                  Empowerment
-                </h2>
-                <p className="text-gray-500 text-base font-normal leading-relaxed lg:text-start text-center">
-                  Through collaborationperse perspectives and strengths are
-                  leveraged to create inclusive environments where everyone has
-                  the opportunity to thrive. This approach not only fosters
-                  personal growth and achievement but also strengthens the
-                  fabric of society.
-                </p>
-              </div>
-              <button className="sm:w-fit w-full px-3.5 py-2 bg-indigo-600 hover:bg-indigo-800 transition-all duration-700 ease-in-out rounded-lg shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.05)] justify-center items-center flex">
-                <span className="px-1.5 text-white text-sm font-medium leading-6">
-                  Get Started
-                </span>
-              </button>
-            </div>
+      <div className="text-center p-8">
+        <h2 className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
+          To Make Great Experiences
+        </h2>
+        <br />
+        <p>Here's Some of our Latest Work...</p>
+        <div className="flex flex-wrap items-center mt-20 text-left text-center">
+          <div className="w-full md:w-2/2 lg:w-1/2 px-4">
             <img
-              className="lg:mx-0 mx-auto h-full rounded-3xl object-cover"
-              src="https://pagedone.io/asset/uploads/1717751272.png"
-              alt="about Us image"
+              src={Seo}
+              alt="gem"
+              className=" inline-block  rounded shadow-lg  border-merino-400"
             />
           </div>
+          <div className="w-full md:w-2/5 lg:w-1/2 px-4 text-center md:text-left lg:pl-12">
+            <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
+              Digital Marketing - SEO - Social Media - Web Development & more...
+            </h3>
+            <p className="sm:text-lg mt-6">
+              martspell was found as a Digital promoting organization in
+              Kolkata. With only a couple of individuals, we are moving towards
+              the long haul and best advanced promoting office in Kolkata,
+              India. We all have had aptitude in different fields for more than
+              4 years.
+              <br />
+              <br />
+              We are the perfect blend of a creator and inventor. This digital
+              marketing agency was awarded the Best Startup Company 2020 for
+              excellence and innovation. We are passionate and thrilled to stay
+              updated about the latest marketing trends and techniques to
+              optimize the key component of conversion rate.
+            </p>
+          </div>
         </div>
-      </section>
+      </div>
+
+      <div className="px-4 py-16 mx-auto sm:max-w-xl  md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="grid max-w-md gap-8 row-gap-10 sm:mx-auto lg:max-w-full lg:grid-cols-3">
+          <div className="flex flex-col sm:flex-row bg-yellow-500 rounded-2xl p-3">
+            <div className="sm:mr-4">
+              <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-green-50">
+                {/*?xml version="1.0" encoding="iso-8859-1"?*/}
+                {/* Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools */}
+                <svg
+                  height="800px"
+                  width="800px"
+                  version="1.1"
+                  id="Layer_1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  viewBox="0 0 511.883 511.883"
+                  xmlSpace="preserve"
+                >
+                  <path
+                    style={{ fill: "#E6E9ED" }}
+                    d="M255.934,117.313c-105.413,0-198.743,54.675-255.934,138.64
+	C57.19,339.894,150.521,394.57,255.934,394.57c105.422,0,198.744-54.676,255.949-138.617
+	C454.678,171.987,361.356,117.313,255.934,117.313z"
+                  />
+                  <path
+                    style={{ fill: "#A0D468" }}
+                    d="M373.244,234.614c0,64.79-52.52,117.31-117.302,117.31c-64.79,0-117.309-52.52-117.309-117.31
+	c0-64.782,52.519-117.301,117.309-117.301C320.724,117.313,373.244,169.832,373.244,234.614z"
+                  />
+                  <path
+                    style={{ fill: "#434A54" }}
+                    d="M266.604,233.927v-20.636h21.331c5.89,0,10.653-4.78,10.653-10.669
+	c0-5.89-4.764-10.654-10.653-10.654h-21.331v-10.669c0-5.89-4.78-10.669-10.67-10.669c-5.889,0-10.661,4.78-10.661,10.669v10.669
+	h-5.335c-18.402,0-24.299,7.53-26.01,13.856c-3.226,11.935,7.037,25.104,31.345,40.163v20.621h-21.331
+	c-5.89,0-10.662,4.779-10.662,10.678c0,5.873,4.772,10.652,10.662,10.652h21.331c0,5.906,4.772,10.654,10.661,10.654
+	c5.89,0,10.67-4.748,10.67-10.654h5.326c18.41,0,24.315-7.529,26.018-13.84C301.182,262.154,290.903,248.985,266.604,233.927z
+	 M245.273,220.149c-3.539-2.718-6.03-4.999-7.71-6.795c0.703-0.031,1.492-0.062,2.375-0.062h5.335V220.149z M271.93,266.607h-5.326
+	v-6.857c3.538,2.717,6.021,5.014,7.709,6.795C273.61,266.591,272.813,266.607,271.93,266.607z"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div>
+              <h6 className="mb-2 font-semibold leading-5">OUR VISION</h6>
+              <p className="mb-3 text-sm text-white">
+                OUR VISION Dedicated channel for a complete branding service...
+                "To be a globally respected corporation that provides
+                best-of-breed business solutions, leveraging technology,
+                delivered by best-in-class people"
+              </p>
+
+              <a
+                href="/"
+                aria-label
+                className="inline-flex items-center font-semibold transition-colors duration-200 text-green-400 hover:text-green-800"
+              >
+                Learn more
+              </a>
+            </div>
+          </div>
+          <div className="flex flex-col sm:flex-row bg-lime-500 rounded-2xl p-3">
+            <div className="sm:mr-4">
+              <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-green-50">
+                {/* Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools */}
+                <svg
+                  width="45px"
+                  height="45px"
+                  viewBox="0 0 48 48"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <defs>
+                    <style
+                      dangerouslySetInnerHTML={{
+                        __html:
+                          ".cls-1{fill:#fa5c66;}.cls-2{fill:#e93d5b;}.cls-3{fill:#face53;}.cls-4{fill:#fabb53;}",
+                      }}
+                    />
+                  </defs>
+                  <title />
+                  <g id="Winner">
+                    <path
+                      className="cls-1"
+                      d="M35.576,11.543,31,17H18.938l-4.55-5.472A2,2,0,0,1,14,10.345V0H36V10.311A2,2,0,0,1,35.576,11.543Z"
+                    />
+                    <rect className="cls-2" height={17} width={10} x={20} />
+                    <path
+                      className="cls-3"
+                      d="M31.062,21H19a3,3,0,0,1,0-6H31a2.916,2.916,0,0,1,3,3A2.882,2.882,0,0,1,31.062,21ZM19.008,17a1,1,0,1,0,0,2h12.05A.885.885,0,0,0,32,18a.919.919,0,0,0-1.012-1Z"
+                    />
+                    <path
+                      className="cls-3"
+                      d="M25.5,19c-.169,0-.333.02-.5.025S24.669,19,24.5,19a14.5,14.5,0,0,0,0,29c.169,0,.333-.02.5-.025s.331.025.5.025a14.5,14.5,0,0,0,0-29Z"
+                    />
+                    <circle className="cls-4" cx="24.5" cy="33.5" r="12.5" />
+                    <circle className="cls-3" cx="24.5" cy="33.5" r="10.5" />
+                    <path
+                      className="cls-4"
+                      d="M25,41a1,1,0,0,1-1-1V29.441l-2.288,2.323a1,1,0,1,1-1.424-1.4l4-4.063A1,1,0,0,1,26,27V40A1,1,0,0,1,25,41Z"
+                    />
+                  </g>
+                </svg>
+              </div>
+            </div>
+            <div>
+              <h6 className="mb-2 font-semibold leading-5">OUR MISSION</h6>
+              <p className="mb-3 text-sm text-white">
+                Put your trust in Martspell today and see how we can help your
+                business... "To achieve our objectives in an environment of
+                fairness, honesty, and courtesy towards our clients, employees,
+                vendors and society"
+              </p>
+
+              <a
+                href="/"
+                aria-label
+                className="inline-flex items-center font-semibold transition-colors duration-200 text-green-400 hover:text-green-800"
+              >
+                Learn more
+              </a>
+            </div>
+          </div>
+          <div className="flex flex-col sm:flex-row bg-red-500 rounded-2xl p-3">
+            <div className="sm:mr-4 ">
+              <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-green-50">
+                {/*?xml version="1.0" encoding="iso-8859-1"?*/}
+                {/* Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools */}
+                <svg
+                  height="45px"
+                  width="45px"
+                  version="1.1"
+                  id="Layer_1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  viewBox="0 0 512 512"
+                  xmlSpace="preserve"
+                >
+                  <path
+                    style={{ fill: "#C20035" }}
+                    d="M492.136,302.982C495.236,287.307,512,272.585,512,256s-16.764-31.307-19.864-46.982
+	c-3.195-16.151,6.599-36.15,0.438-51.01c-6.27-15.12-27.399-22.307-36.396-35.747c-9.066-13.542-7.676-35.797-19.157-47.28
+	s-33.737-10.092-47.28-19.157c-13.44-8.998-20.627-30.127-35.747-36.397c-14.861-6.163-34.86,3.632-51.011,0.438
+	C287.307,16.764,272.585,0,256,0l-65.501,256L256,512c16.585,0,31.307-16.764,46.982-19.864c16.151-3.195,36.15,6.599,51.01,0.438
+	c15.12-6.27,22.307-27.399,35.747-36.396c13.542-9.066,35.797-7.676,47.28-19.157c11.482-11.482,10.092-33.737,19.157-47.28
+	c8.998-13.44,30.127-20.627,36.397-35.747C498.735,339.133,488.94,319.134,492.136,302.982z"
+                  />
+                  <g>
+                    <path
+                      style={{ fill: "#FF0F47" }}
+                      d="M209.018,19.864c-16.151,3.195-36.15-6.599-51.01-0.438c-15.12,6.27-22.307,27.399-35.747,36.396
+		c-13.542,9.066-35.797,7.676-47.28,19.157c-11.482,11.482-10.092,33.737-19.157,47.28c-8.998,13.44-30.127,20.627-36.397,35.747
+		c-6.163,14.861,3.632,34.86,0.438,51.011C16.764,224.693,0,239.415,0,256s16.764,31.307,19.864,46.982
+		c3.195,16.151-6.599,36.15-0.438,51.01c6.27,15.12,27.399,22.307,36.396,35.747c9.066,13.542,7.676,35.797,19.157,47.28
+		c11.481,11.482,33.737,10.092,47.28,19.157c13.44,8.998,20.627,30.127,35.747,36.397c14.861,6.163,34.86-3.632,51.011-0.438
+		C224.693,495.236,239.415,512,256,512V0C239.415,0,224.693,16.764,209.018,19.864z"
+                    />
+                    <path
+                      style={{ fill: "#FF0F47" }}
+                      d="M256,48.58c114.554,0,207.42,92.866,207.42,207.42S370.554,463.42,256,463.42L175.337,256L256,48.58
+		z"
+                    />
+                  </g>
+                  <path
+                    style={{ fill: "#FF6176" }}
+                    d="M48.58,256c0,114.554,92.866,207.42,207.42,207.42V48.58C141.446,48.58,48.58,141.446,48.58,256z"
+                  />
+                  <circle
+                    style={{ fill: "#FFA89E" }}
+                    cx={256}
+                    cy={256}
+                    r="174.67"
+                  />
+                  <polygon
+                    style={{ fill: "#FF6176" }}
+                    points="335.693,205.575 256.218,165.095 213.389,205.575 170.554,165.095 127.727,250.618 
+	231.71,294.285 335.693,250.618 "
+                  />
+                  <polygon
+                    style={{ fill: "#C20035" }}
+                    points="341.883,165.095 341.883,205.575 321.829,205.575 293.552,185.335 309.362,165.095 "
+                  />
+                  <g>
+                    <polygon
+                      style={{ fill: "#FF0F47" }}
+                      points="256.218,165.095 213.392,250.618 170.554,165.095 	"
+                    />
+                    <polygon
+                      style={{ fill: "#FF0F47" }}
+                      points="321.829,205.575 299.504,250.618 256.218,165.095 309.362,165.095 	"
+                    />
+                    <polygon
+                      style={{ fill: "#FF0F47" }}
+                      points="341.883,165.095 384.273,250.618 347.338,272.452 335.693,250.618 321.829,205.575 	"
+                    />
+                  </g>
+                  <polygon
+                    style={{ fill: "#C20035" }}
+                    points="335.693,250.618 310.911,269.457 299.504,250.618 213.392,250.618 196.095,300.243 
+	256,387.439 384.273,250.618 "
+                  />
+                  <g>
+                    <polygon
+                      style={{ fill: "#FF0F47" }}
+                      points="213.392,250.618 256,387.439 127.727,250.618 	"
+                    />
+                    <polygon
+                      style={{ fill: "#FF0F47" }}
+                      points="335.693,250.618 256,387.439 299.504,250.618 	"
+                    />
+                  </g>
+                </svg>
+              </div>
+            </div>
+            <div>
+              <h6 className="mb-2 font-semibold leading-5">OUR VALUE</h6>
+              <p className="mb-3 text-sm text-white">
+                "Martspell Stands for Creation and innovation. We deliver the
+                exact vision of our customers. We value every member of our team
+                as their hard work and dedication make us stand strong"
+              </p>
+
+              <a
+                href="/"
+                aria-label
+                className="inline-flex items-center font-semibold transition-colors duration-200 text-green-400 hover:text-green-800"
+              >
+                Learn more
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="text-center p-8">
+        {" "}
+        <div className="flex flex-wrap items-center mt-20 sm:text-left text-center">
+          <div className="w-full md:w-3/5 lg:w-1/2 px-4">
+            <img
+              src={Containt}
+              alt="bulk editing"
+              className="inline-block rounded shadow-lg"
+            />
+          </div>
+          <div className="w-full md:w-2/5 lg:w-1/2 px-4 md:order-first text-center md:text-left lg:pr-12">
+            <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-6xl">
+              Setting up ArtworkPerfect for Screen!
+            </h3>
+            <p className="sm:text-lg mt-6">
+              We offer digital technology at various levels under one roof.
+              Provide expertise in Website Development, Social media marketing &
+              management, PPC & SEO, Google My Business, E-Commerce Solutions,
+              Graphic Support, 360 Degree Shoot, Professional Photography &
+              other Advertising activities combine with full flexibility and
+              customization control.
+            </p>
+            <button className="p-3 bg-amber-500 rounded-3xl mt-[10px]">
+              DISCOVER NOW
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
