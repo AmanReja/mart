0e343f89fc729fb738aadfe05a2c3da0e49aae7d
Aman Reja <amnmolla742@gmail.com>
2025-07-16 15:51:12 +0530

    fix

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -5,7 +5,7 @@ const Services = () => {
     <>
       <main id="content">
         {/* Hero */}
-        <div className="overflow-hidden bg-neutral-900">
+        <div className="overflow-hidden ">
           <div className="max-w-5xl mx-auto px-4 xl:px-0 pt-24 lg:pt-32 pb-24">
             <h1 className="font-semibold text-white text-5xl md:text-6xl">
               <span className="text-[#ff0] ">Martspell</span> Transforming ideas
